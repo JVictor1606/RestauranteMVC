12afa820ffa2b10a4d42688d98cf13ddedbd8819
João Victor S. Matos <jvictorsousa2015@gmail.com>
2023-04-05 18:55:34 -0300

    Criação do DbContext e do Model

--- Bra.ProductApi/DbContexts/AppDbContext.cs
+++ Bra.ProductApi/DbContexts/AppDbContext.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bra.ProductApi.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bra.ProductApi.DbContexts
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
 
+        }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
